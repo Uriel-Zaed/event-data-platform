8af4e7290ebd44e91c42889d7e09a7ebb6a5110e
Uriel Zaed <urielzaed33@gmail.com>
2026-02-17 20:36:20 +0200

    pyspark session added to util

---
+++ src/utils/spark.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+def get_spark(app_name: str, shuffle_partitions: int = 10) -> SparkSession:
+    spark = (
+        SparkSession.builder
+        .appName(app_name)
+        .master("local[*]")
+        .getOrCreate()
+    )
+    spark.conf.set("spark.sql.shuffle.partitions", str(shuffle_partitions))
+    spark.sparkContext.setLogLevel("WARN")
+    return spark
